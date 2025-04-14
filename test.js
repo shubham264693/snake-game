9200e7b3ffa3579c61f47834ce8c6e0088a09f49
shubham264693 <shubhamkale008@gmail.com>
2025-04-14 20:11:49 +0530

    test file added new

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
-function test(){
-    return new Promise((resolve,reject)=>{
-        resolve('success')
-    })
+function test(...numbers){
+    console.log(numbers)
+   let sum = 0
+    for(let i=0;i<numbers.length;i++){
+        console.log(numbers[i])
+        sum += numbers[i];
+   }
+   return sum
 }
+let arr = [1,2,3,4]
+console.log(typeof arr);
 
-let getData = test();
+let getData = test(...arr);
 console.log(getData,'Print data details');
